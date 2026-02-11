7cc9921e327ce7baaad180e9d4a901bf91f27e10
TuMankito77 <bolivaritoviva@gmail.com>
2026-02-10 21:40:44 -0500

    Patrol location index selection
    - Added an Enum that allows the choosing of different patrol point indices in different ways.
    - Added a new function for each Enum type of patrol location index selection.

--- ShooterPlatformerUE/Source/LyraGame/Enemy/PatrolPointSelector.cpp
+++ ShooterPlatformerUE/Source/LyraGame/Enemy/PatrolPointSelector.cpp
@@ -29,9 +29,95 @@ EBTNodeResult::Type UPatrolPointSelector::ExecuteTask(UBehaviorTreeComponent& Ow
 
 	if (ensure(BlackboardComp))
 	{
-		BlackboardComp->SetValueAsVector(FName("TargetLocation"), PatrolPath->GetPatrolPointLocation(0));
+		int PatrolLocationIndex = BlackboardComp->GetValueAsInt(FName("PatrolLocationIndex"));
+
+		if (PatrolLocationIndex == -1)
+		{
+			PatrolLocationIndex = 0;
+			BlackboardComp->SetValueAsVector(FName("TargetLocation"), PatrolPath->GetPatrolPointLocation(PatrolLocationIndex));
+			BlackboardComp->SetValueAsInt(FName("PatrolLocationIndex"), PatrolLocationIndex);
+		}
+		else
+		{
+			SelectNextPatrolPoint(*BlackboardComp, *PatrolPath);
+		}
 	}
 
 
 	return EBTNodeResult::Succeeded;
+}
+
+void UPatrolPointSelector::SelectNextPatrolPoint(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath)
+{
+	int NextPatrolPointIndex = 0;
+
+	switch (PointSelectionType)
+	{
+		case PointSelectionType::IncreaseDecreaseInOrder:
+		{
+			NextPatrolPointIndex = SelectWithIncreaseDecreaseOrder(BlackboardComp, PatrolPath);
+			break;
+		}
+
+		case PointSelectionType::IncreaseLoop:
+		{
+			NextPatrolPointIndex = SelectWithIncreaseLoop(BlackboardComp, PatrolPath);
+			break;
+		}
+
+		case PointSelectionType::DecreaseLoop:
+		{
+			NextPatrolPointIndex = SelectWithDecreaseLoop(BlackboardComp, PatrolPath);
+			break;
+		}
+
+		case PointSelectionType::Random:
+		{
+			NextPatrolPointIndex = SelectWithRamdom(BlackboardComp, PatrolPath);
+			break;
+		}
+
+		default:
+		{
+			break;
+		}
+	}
+
+	BlackboardComp.SetValueAsVector(FName("TargetLocation"), PatrolPath.GetPatrolPointLocation(NextPatrolPointIndex));
+	BlackboardComp.SetValueAsInt(FName("PatrolLocationIndex"), NextPatrolPointIndex);
+}
+
+int UPatrolPointSelector::SelectWithIncreaseDecreaseOrder(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath)
+{
+	int PatrolLocationIndex = BlackboardComp.GetValueAsInt(FName("PatrolLocationIndex"));
+
+	if (PatrolLocationIndex == PatrolPath.PatrolPointLocations.Num() - 1 ||
+		PatrolLocationIndex == 0)
+	{
+		IncreaseDecreaseSense *= -1;
+	}
+	else
+	{
+		PatrolLocationIndex += IncreaseDecreaseSense;
+	}
+
+	return PatrolLocationIndex;
+}
+int UPatrolPointSelector::SelectWithIncreaseLoop(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath)
+{
+	int PatrolLocationIndex = BlackboardComp.GetValueAsInt(FName("PatrolLocationIndex"));
+	PatrolLocationIndex++;
+	PatrolLocationIndex %= PatrolPath.PatrolPointLocations.Num();
+	return PatrolLocationIndex;
+}
+int UPatrolPointSelector::SelectWithDecreaseLoop(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath)
+{
+	int PatrolLocationIndex = BlackboardComp.GetValueAsInt(FName("PatrolLocationIndex"));
+	PatrolLocationIndex--;
+	PatrolLocationIndex = FMath::Abs(PatrolLocationIndex) % PatrolPath.PatrolPointLocations.Num();
+	return PatrolLocationIndex;
+}
+int UPatrolPointSelector::SelectWithRamdom(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath)
+{
+	return FMath::RandRange((int)0, (int)PatrolPath.PatrolPointLocations.Num());
 }
--- ShooterPlatformerUE/Source/LyraGame/Enemy/PatrolPointSelector.h
+++ ShooterPlatformerUE/Source/LyraGame/Enemy/PatrolPointSelector.h
@@ -7,6 +7,16 @@
 #include "PatrolPointSelector.generated.h"
 
 class AAIControllerBaseEnemy;
+class APatrolPath;
+
+UENUM()
+enum class PointSelectionType
+{
+	IncreaseDecreaseInOrder UMETA(DisplayName = "Increase Decrease in order"),
+	IncreaseLoop UMETA(DisplayName = "Increase loop"),
+	DecreaseLoop UMETA(DisplayName = "Decrease loop"),
+	Random UMETA(DisplayName = "Random")
+};
 
 UCLASS()
 class LYRAGAME_API UPatrolPointSelector : public UBTTaskNode
@@ -14,8 +24,19 @@ class LYRAGAME_API UPatrolPointSelector : public UBTTaskNode
 	GENERATED_BODY()
 	
 private:
+	UPROPERTY(EditAnywhere)
+	PointSelectionType PointSelectionType = PointSelectionType::IncreaseDecreaseInOrder;
+
 	TObjectPtr<AAIControllerBaseEnemy> AICBaseEnemy = nullptr;
+	int IncreaseDecreaseSense = 1;
 
 public:
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+	void SelectNextPatrolPoint(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath);
+	int SelectWithIncreaseDecreaseOrder(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath);
+	int SelectWithIncreaseLoop(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath);
+	int SelectWithDecreaseLoop(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath);
+	int SelectWithRamdom(UBlackboardComponent& BlackboardComp, APatrolPath& PatrolPath);
 };
